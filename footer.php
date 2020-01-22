b44d93edf6f85630925c87f180cbb7beafef1847
170 <joynext10126@gmail.com>
2020-01-22 15:39:34 +0000

    HarryStyles

---
+++ footer.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+a{
+	color:black;
+}
+.footer {
+   position: relative;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color:beige;
+   color: black;
+   text-align: center;
+   border:papayawhip solid; 
+}
+ul.soc-media li {
+    display:inline;
+    padding:1.2vw;
+}
+.fa {
+  font-size: 32px;
+  width: 70px;
+  text-align: center;
+  margin: 5px 2px;
+  border-radius: 50%;
+  padding:20px;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+.fa-snapchat-ghost {
+  background: #fffc00;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+</style>
+</head>
+<body>
+<div class="footer">
+  <ul class="soc-media">
+    <li><a href="https://twitter.com/harry_styles" class="fa fa-twitter"></a></li>
+    <li><a href="https://www.facebook.com/harrystyles/" class="fa fa-facebook"></a></li>
+    <li><a href="https://www.youtube.com/channel/UCZFWPqqPkFlNwIxcpsLOwew" class="fa fa-youtube"></a></li>
+	<li><a href="https://www.snapchat.com/" class="fa fa-snapchat-ghost"></a></li>
+  </ul>
+  <p>Copyright 2018 Atlantic Records</p>
+  <a href="https://www.gov.uk/data-protection">Terms</a>
+</div>
+
+</body>
+</html> 
